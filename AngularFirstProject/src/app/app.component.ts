6784a09f38a540adeb2487bf716d940c9f6b6843
Clement Lin <x4383lin@gmail.com>
2017-11-02 11:01:19 -0400

    3. Master/Details

--- AngularFirstProject/src/app/app.component.ts
+++ AngularFirstProject/src/app/app.component.ts
@@ -5,6 +5,20 @@ export class Hero {
   name: string;
 }
 
+const HEROES: Hero[] = [
+  { id : 11, name: 'Mr.Nice'},
+  { id : 12, name: 'Narco'},
+  { id : 13, name: 'Bombasto'},
+  { id : 14, name: 'Celeritas'},
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
+
 /*
     Component Decorator: 
       requires a selector and template/templateUrl
@@ -17,8 +31,17 @@ export class Hero {
 })
 export class AppComponent {
   title = 'Clement app';
-  hero: Hero ={
-   id: 1,
-   name: 'Windstrom'
+  // hero: Hero ={
+  //  id: 1,
+  //  name: 'Windstrom'
+  // }
+  selectedHero: Hero;
+  heroes = HEROES;
+  onSelect(hero: Hero): void {   //??????????
+    this.selectedHero = hero;
   }
 }
+
+
+
+
